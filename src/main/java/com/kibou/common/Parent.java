30eb2e9c0bad0bbc160c0aff75ad15e33ef4cd1c
shifeng <763091466@qq.com>
2016-08-28 22:30:37 +0800

    do static resource test

--- src/main/java/com/kibou/common/Parent.java
+++ src/main/java/com/kibou/common/Parent.java
@@ -1,9 +1,7 @@
 package com.kibou.common;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
+//@Configuration
+//@ComponentScan
 public class Parent {
 }
--- src/main/java/com/kibou/web/controller/HelloWorldController.java
+++ src/main/java/com/kibou/web/controller/HelloWorldController.java
@@ -6,8 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 	
-	@RequestMapping //ROOT, localhost:8080
+	@RequestMapping("/") //
+	// PS: Cautious!!!! XXX
+	// @RequestMapping("")  如果配置成这样的话 好像和 servlet中的ulr-pattern=/那样拦截所有的请求
+	// 所以致使静态资源的请求都被拦截,所有都会走这个请求里面
 	public String home(){
 		return "Hello World";
 	}
 }
+
